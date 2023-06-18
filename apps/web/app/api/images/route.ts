a3927edfb46313a05afe69c923e7afcdabb97a67
Wilmer Cantillo <will.canti2697@gmail.com>
2023-06-18 13:40:40 -0500

    fix: debugging image upload in stg

--- apps/web/app/api/images/route.ts
+++ apps/web/app/api/images/route.ts
@@ -5,10 +5,14 @@ export async function POST(req: Request) {
   const formData = await req.formData()
   const image = formData.get('image') as File
   const buffer = Buffer.from(await image.arrayBuffer())
+  const saveFileFirstTryResponse = await fs
+    .writeFile(`public/${image.name}`, buffer)
+    .catch((e) => new Error(e))
+  console.log({ saveFileFirstTryResponse })
   const saveFileResponse = await fs.writeFile(`public/books/${image.name}`, buffer).catch((e) => new Error(e))
 
   if (saveFileResponse instanceof Error) {
-    console.error(saveFileResponse)
+    console.error({ saveFileResponse })
     const errors: ResponseError = [
       {
         title: 'Server error',
